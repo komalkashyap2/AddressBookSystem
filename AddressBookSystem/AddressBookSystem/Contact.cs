ac13cee9fa740178858298bb147b40f648ab0e7c
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-04 19:51:47 +0530

    [KOMAL KASHYAP] Added Create a contact program

---
+++ AddressBookSystem/AddressBookSystem/Contact.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+      class Contact
+      {
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string PhoneNumber { get; set; }
+            public string[] Addresses { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string zip { get; set; }
+            public string Email { get; set; }
+        }
+
+        public static List<Person> People = new List<Person>();
+
+        private static void AddPerson()
+        {
+            Person person = new Person();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Address 1: ");
+            string[] addresses = new string[2];
+            addresses[0] = Console.ReadLine();
+
+            Console.Write("Enter Address 2 (Optional): ");
+            addresses[1] = Console.ReadLine();
+            person.Addresses = addresses;
+
+            Console.Write("Enter City: ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter zip: ");
+            person.zip = Console.ReadLine();
+
+            Console.Write("Enter  Email: ");
+            person.Email = Console.ReadLine();
+
+            People.Add(person);
+        }
+        private static void PrintPerson(Person person)
+        {
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("Address 1: " + person.Addresses[0]);
+            Console.WriteLine("Address 2: " + person.Addresses[1]);
+            Console.WriteLine("-------------------------------------------");
+        }
+    }
+}
